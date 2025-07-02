f53e473b047cbab061f62599413033218b788713
MateuszGolebiewski591 <m.golebiewski591@gmail.com>
2025-07-02 18:09:16 +0100

    Changed default to 30000 iterations.

--- run_shell_video.py
+++ run_shell_video.py
@@ -11,7 +11,7 @@ for lmbda in [0.004]:  # Optionally, you can try: 0.003, 0.002, 0.001, 0.0005
 "--lod", "0",
 "--voxel_size", "0.001",
 "--update_init_factor", "4",
-"--iterations", "10",
+"--iterations", "30000",
 "-m", "outputs/videos/carSubset/0.004",
 "--lmbda", "0.004",
 "--h", "0.1",
